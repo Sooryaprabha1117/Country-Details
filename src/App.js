204f176d5332e2f6adae2a7e7197d2921157058f
Sooryaprabha1117 <prabhasoorya17@gmail.com>
2024-09-23 17:51:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CountryInformation from "./country/CountryInformation"
+
+function App() {
+  return (
+    <div className="App">
+      <CountryInformation/>
+    </div>
+  );
+}
+
+export default App;
